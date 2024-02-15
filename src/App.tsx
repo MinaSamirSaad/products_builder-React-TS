bc6b67ecc305f50d1d0182898d11bfbbddc82be2
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-15 02:48:46 +0200

    add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { useState } from 'react'
 import ProductCard from './components/ProductCard'
+import Modal from './components/UI/Modal';
 import { productList } from './data'
+import Button from './components/UI/Button';
 
 function App() {
-  const renderProductList = productList.map((product) => <ProductCard key={product.id} product = {product}/>);
+  // ------------------ State ------------------
+  const [isOpen, setIsOpen] = useState(false)
+
+  // ------------------ Functions ------------------
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  // ------------------ Render ------------------
+  const renderProductList = productList.map((product) => <ProductCard key={product.id} product={product} />);
   return (
     <main className='container'>
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 m-5 rounded-md p-2'>
-      {renderProductList}
-    </div>
+      <Button onClick={openModal} width='w-fit' className='bg-green-700 hover:bg-green-800'>ADD ITEM</Button>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 m-5 rounded-md p-2'>
+        {renderProductList}
+      </div>
+      <Modal isOpen={isOpen} closeModal={closeModal} >
+        <div className='flex items-center space-x-3'>
+          <Button onClick={closeModal} className='bg-indigo-700 hover:bg-indigo-800'>Submit</Button>
+          <Button onClick={closeModal} className='bg-gray-400 hover:bg-gray-800'>Cancel</Button>
+        </div>
+      </Modal>
     </main>
   )
 }
--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -6,6 +6,6 @@ className?: string;
 width?: 'w-full' | 'w-fit'
 }
 const Button = ({children,className,width = 'w-full', ...rest}:IProps) => {
-  return <button className={`${className} ${width} p-2 rounded-md`} {...rest}>{children}</button>
+  return <button className={`${className} ${width} p-2 rounded-md text-white`} {...rest}>{children}</button>
 }
 export default Button;
