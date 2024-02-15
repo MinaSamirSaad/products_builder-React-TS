42d2437b0119c22e0b6d2b033d84e158b4aab086
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-15 04:28:34 +0200

    Enhance modal, input, button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import ProductCard from './components/ProductCard'
 import Modal from './components/UI/Modal';
-import { productList } from './data'
+import { productList, formInputsList } from './data'
 import Button from './components/UI/Button';
+import Input from './components/UI/Input';
 
 function App() {
   // ------------------ State ------------------
@@ -19,6 +20,10 @@ function App() {
 
   // ------------------ Render ------------------
   const renderProductList = productList.map((product) => <ProductCard key={product.id} product={product} />);
+  const renderFormInputs = formInputsList.map(input => <div key={input.id} className='flex flex-col '>
+    <label htmlFor={input.id} className='mb-[1px] text-sm font-medium text-gray-700'>{input.label}</label>
+    <Input id={input.id} name={input.name} type={input.type}/>
+  </div>)
   return (
     <main className='container'>
       <Button onClick={openModal} width='w-fit' className='bg-green-700 hover:bg-green-800'>ADD ITEM</Button>
@@ -26,10 +31,13 @@ function App() {
         {renderProductList}
       </div>
       <Modal isOpen={isOpen} closeModal={closeModal} >
+        <form className='space-y-3'>
+        {renderFormInputs}
         <div className='flex items-center space-x-3'>
           <Button onClick={closeModal} className='bg-indigo-700 hover:bg-indigo-800'>Submit</Button>
-          <Button onClick={closeModal} className='bg-gray-400 hover:bg-gray-800'>Cancel</Button>
+          <Button onClick={closeModal} className='bg-gray-400 hover:bg-gray-500'>Cancel</Button>
         </div>
+        </form>
       </Modal>
     </main>
   )
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,32 +1,32 @@
-import { IProduct } from "../../interfaces/data";
+import { IProduct } from "../../interfaces";
 import { textSlicer } from "../../utils/functions";
 import Image from "../Image";
 import Button from "../UI/Button";
 interface IProps {
-  product:IProduct
+  product: IProduct
 }
-const ProductCard = ({product}:IProps) => {
-  const {title,description,imageURL, category, price} = product;
+const ProductCard = ({ product }: IProps) => {
+  const { title, description, imageURL, category, price } = product;
   return (
     <div className="mx-w-sm md:max-w-lg mx-auto md:mx-0 border rounded p-2 flex flex-col">
-        <Image  imageUrl={imageURL} alt="car photo" className="rounded-md"/>
-        <h3>{title}</h3>
-        <p>{textSlicer(description, 50)}</p>
-        <div className="flex items-center my-4 space-x-1">
-            <span className="w-5 h-5 bg-amber-800 rounded-full cursor-pointer"></span>
-            <span className="w-5 h-5 bg-indigo-600 rounded-full cursor-pointer"></span>
-            <span className="w-5 h-5 bg-orange-400 rounded-full cursor-pointer"></span>
-            <span className="w-5 h-5 bg-green-500 rounded-full cursor-pointer"></span>
-        </div>
-        <div className=" flex justify-between items-center">
-            <span className="">${price}</span>
-            <Image  imageUrl={category.imageURL} alt={category.name} className="w-10 h-10 rounded-full object-cover"/>
-        </div>
+      <Image imageUrl={imageURL} alt="car photo" className="rounded-md" />
+      <h3>{title}</h3>
+      <p>{textSlicer(description, 50)}</p>
+      <div className="flex items-center my-4 space-x-1">
+        <span className="w-5 h-5 bg-amber-800 rounded-full cursor-pointer"></span>
+        <span className="w-5 h-5 bg-indigo-600 rounded-full cursor-pointer"></span>
+        <span className="w-5 h-5 bg-orange-400 rounded-full cursor-pointer"></span>
+        <span className="w-5 h-5 bg-green-500 rounded-full cursor-pointer"></span>
+      </div>
+      <div className=" flex justify-between items-center">
+        <span className="">${price}</span>
+        <Image imageUrl={category.imageURL} alt={category.name} className="w-10 h-10 rounded-full object-cover" />
+      </div>
 
-        <div className="flex items-center justify-between space-x-1 my-5 text-white ">
-            <Button className="bg-indigo-700">Edit</Button>
-            <Button className="bg-red-700">Delete</Button>
-        </div>
+      <div className="flex items-center justify-between space-x-1 my-5 text-white ">
+        <Button className="bg-indigo-700">Edit</Button>
+        <Button className="bg-red-700">Delete</Button>
+      </div>
     </div>
   )
 }
