d1e7cd318e91220a5a374953c459c3a40ebecb82
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-03-16 01:22:29 +0200

    add final update in the project

--- src/components/UI/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-interface IProps {
-    message: string;
-}
-const ErrorMessage = ({message}:IProps) => {
-  return (
-    message && <p className="block text-red-700 text-fw-semibold text-sm">{message}</p>
-  )
-}
-export default ErrorMessage;
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,10 +1,20 @@
 /**
- * 
- * @param {string} text - long text to be sliced
- * @param {number} [max = 50] - maximum length of the sliced text
- * @returns the sliced text, with an ellipsis (...) appended if truncated.
+ *
+ * @param {string} txt - The input text to be sliced.
+ * @param {number} [max=50] - The maximum length before truncation.
+ * @returns The sliced text, with an ellipsis (...) appended if truncated.
  */
-export function textSlicer(text: string, max: number = 50): string {
-    if (text.length >= max) return `${text.slice(0, max)} ...`;
-    return text;
+export function txtSlicer(txt: string, max: number = 80) {
+  if (txt.length >= max) return `${txt.slice(0, max)}...`;
+  return txt;
+}
+
+/**
+ *
+ * @param {string} x - The numeric string to be formatted.
+ * @returns {string} A formatted version of the input numeric string with commas as thousand separators.
+ *
+ */
+export function numberWithCommas(x: string): string {
+  return x?.toString()?.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
